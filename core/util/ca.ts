2a14cd80555730204172d55342e218a8f0dcd62f
Karl G <karl@grindleyfamily.com>
2026-02-26 19:58:31 -0500

    Fix VS Code TLS trust for fetch/undici with system CAs

--- core/util/ca.ts
+++ core/util/ca.ts
@@ -1,10 +1,26 @@
 import { globalAgent } from "https";
+import * as tls from "tls";
 
 // @ts-ignore
 import { systemCertsAsync } from "system-ca";
 
 export async function setupCa() {
   try {
+    const systemCerts = await systemCertsAsync();
+
+    // Node's global fetch() uses undici and reads from tls default certificates,
+    // not https.globalAgent. Merge system certs into tls defaults so both stacks
+    // trust the OS certificate store.
+    const getDefaultCerts = (tls as any).getCACertificates;
+    const setDefaultCerts = (tls as any).setDefaultCACertificates;
+    if (
+      typeof getDefaultCerts === "function" &&
+      typeof setDefaultCerts === "function"
+    ) {
+      const defaultCerts = getDefaultCerts("default");
+      setDefaultCerts([...defaultCerts, ...systemCerts]);
+    }
+
     switch (process.platform) {
       case "darwin":
         // https://www.npmjs.com/package/mac-ca#usage
@@ -18,7 +34,7 @@ export async function setupCa() {
         break;
       default:
         // https://www.npmjs.com/package/system-ca
-        globalAgent.options.ca = await systemCertsAsync();
+        globalAgent.options.ca = systemCerts;
         break;
     }
   } catch (e) {
